3b0d7f6a557b108f683a274df29ce90795d47b6c
Santiago Castellanos <scastel1@its.jnj.com>
2026-03-22 00:43:38 -0500

    Fix logout

--- leaderboard/app/api/auth/logout/route.ts
+++ leaderboard/app/api/auth/logout/route.ts
@@ -3,9 +3,9 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const cognitoDomain = `https://${process.env.COGNITO_DOMAIN || "leaderboard-dev"}.auth.${process.env.APP_AWS_REGION || "us-west-2"}.amazoncognito.com`;
   const clientId = process.env.COGNITO_CLIENT_ID || "";
-  const logoutUri = encodeURIComponent(`${process.env.NEXTAUTH_URL || ""}/admin/login`);
+  const redirectUri = encodeURIComponent(`${process.env.NEXTAUTH_URL || ""}/admin/login`);
 
-  const cognitoLogoutUrl = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${logoutUri}`;
+  const cognitoLogoutUrl = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${redirectUri}&redirect_uri=${redirectUri}`;
 
   return NextResponse.redirect(cognitoLogoutUrl);
 }
